310c7626eea0251740de3b1c7fbab50816dd78b8
Usman Sagir Usman <walidsagir8@gmail.com>
2026-06-01 03:42:23 +0100

    Update agent.py

--- agent.py
+++ agent.py
@@ -119,7 +119,7 @@ class SalaryAgentPayer:
   }'
   """)
    llm = ChatGoogleGenerativeAI(
-   model = "gemini-2.5-flash",
+   model = "gemini-3.1-flash-lite",
    api_key = llm_api
    )
    self.checkpointer = InMemorySaver()
