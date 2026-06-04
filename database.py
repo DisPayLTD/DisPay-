b9428fc7457f5e6aad02c15be7a04a9f27fc5a40
Usman Sagir Usman <walidsagir8@gmail.com>
2026-06-04 02:38:08 +0100

    Create database.py

---
+++ database.py
@@ -0,0 +1,41 @@
+from sqlalchemy import String,Float,create_engine,Column,Integer
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import declarative_base
+
+SQL_URL = "sqlite:///./remitron.db"
+
+
+
+Base = declarative_base()
+
+
+class Users(Base):
+	__tablename__ = "users"
+	id = Column(Integer,primary_key = True)
+	first_name = Column(String,default = "Unknown")
+	last_name = Column(String,default = "Unknown")
+	email = Column(String)
+	nin = Column(String)
+	phone = Column(String)
+	bvn = Column(String)
+	customer_id = Column(String)
+	account_number = Column(String)
+	bank_name = Column(String)
+	wallet_balance = Column(Float, default= 0.0)
+
+engine = create_engine(SQL_URL)
+sessionLocal = sessionmaker(autocommit = False,autoflush = False,bind = engine)
+
+
+def get_db():
+	db = sessionLocal()
+	try:
+		yield db
+	finally:
+		db.close()
+
+def init_db():
+	Base.metadata.create_all(bind = engine)
+	
+	
+
