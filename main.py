8ecc0adf7f3b43ab144bc1839212c50299cc9daf
Usman Sagir Usman <walidsagir8@gmail.com>
2026-06-07 16:00:52 +0100

    Update main.py

--- main.py
+++ main.py
@@ -130,12 +130,12 @@ def get_user_data(request: Request, db: Session = Depends(get_db)):
     user = db.query(Users).filter(Users.id == user_id).first()
     
     if not user:
-        return {
-            "status": "failed",
-            "message": "User not found",
-            "url": "/auth"
-        }
-    set_user_id({"user_id":user_id})
+		return {
+			"status": "failed",
+			"message": "User not found",
+			"url": "/auth"
+		}
+	set_user_id({"user_id":user_id})
 	print("walid we set the userid and it is: ",user_id)
     return {
         "status": "success",
