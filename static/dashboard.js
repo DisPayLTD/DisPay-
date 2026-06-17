8869ac46d1111fd2d99d5ee5ca4cc9c0fd5845a2
Usman Sagir Usman <walidsagir8@gmail.com>
2026-06-17 05:48:55 +0100

    Update dashboard.js

--- static/dashboard.js
+++ static/dashboard.js
@@ -114,9 +114,9 @@ function fetchBalance(){
             
             if(data.status === "success"){  
                 const balance = data.balance || 0.00;
-                // Format with commas: ₦10,000.00
-                document.getElementById('walletBalance').textContent = 
-                    `₦${balance.toLocaleString('en-NG', {minimumFractionDigits: 2})}`;
+                alert("balance: ", data.message);
+                document.getElementById('walletBalance').textContent = (user.balance || 0).toFixed(2);
+    ;
             }
         }catch(error){
             console.error("Balance fetch error:", error);  // Use console.error, not alert
