21483243218431e955abae0e9b86a93f4c76ee3b
Usman Sagir Usman <walidsagir8@gmail.com>
2026-06-18 16:56:23 +0100

    Update agent.py

--- agent.py
+++ agent.py
@@ -8,7 +8,7 @@ from langchain_core.utils.uuid import uuid7
 from tabulate import tabulate
 import os
 from context import get_db_session,get_user_id
-from database import Users
+from database import Users, Transfers 
 import pandas as pd
 import uuid
 
