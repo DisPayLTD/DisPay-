2a8a3322b473a97a275f5be82486bad45a164442
Usman Sagir Usman <walidsagir8@gmail.com>
2026-06-21 04:02:09 +0100

    Update dashboard.js

--- static/dashboard.js
+++ static/dashboard.js
@@ -83,7 +83,9 @@ function populateDashboard(user) {
     document.getElementById('userName').textContent = `${user.first_name} ${user.last_name}`;
     document.getElementById('userEmail').textContent = user.email;
     //document.getElementById('walletBalance').textContent = (user.wallet_balance || 0).toFixed(2);
-    fetchBalance();
+    if (user.has_wallet){
+        fetchBalance();
+    }
     
     // Account number section
     const accountNumberDiv = document.getElementById('accountNumberDiv');
