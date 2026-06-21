8d3c6ac35beea5c34ac9f55d250d9e8f10a5a4e3
Usman Sagir Usman <walidsagir8@gmail.com>
2026-06-21 04:15:07 +0100

    Update dashboard.js

--- static/dashboard.js
+++ static/dashboard.js
@@ -3,7 +3,6 @@ let userEmail = '';
 let isAuthenticated = true;
 let pendingPaymentData = null;
 
-
 function getCsrfToken(){
     const value = `; ${document.cookie}`;
     const data = value.split("; csrftoken=")
