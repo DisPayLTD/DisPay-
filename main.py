f47a33a83b27cc33a9ff94ef1ba2a76c6ba64fb4
Usman Sagir Usman <walidsagir8@gmail.com>
2026-08-02 08:38:42 +0100

    Update main.py

--- main.py
+++ main.py
@@ -791,7 +791,7 @@ def generate_account_number(req: Request, db: Session = Depends(get_db)):
         "dob": dob,
         "address": address,
         "gender": gender,
-        "beneficiary_account":"1040622844"
+        "beneficiary_account":"6550842542"
     }
     url = "https://api-d.squadco.com/virtual-account"
 
