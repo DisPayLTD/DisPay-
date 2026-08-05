60bcc8a02b74ae88abfe3bf3c280c826f64702c8
Usman Sagir Usman <walidsagir8@gmail.com>
2026-08-05 13:39:18 +0100

    Create sw.js

---
+++ static/sw.js
@@ -0,0 +1,16 @@
+// Minimal Service Worker for PWABuilder Compliance
+const CACHE_NAME = 'dispay-pwa-v1';
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
