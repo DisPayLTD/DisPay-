d066285fbf776aad94ff8fa8a11521ab3b039aea
Usman Sagir Usman <walidsagir8@gmail.com>
2026-08-30 15:25:14 +0100

    Update dashboard.js

--- static/dashboard.js
+++ static/dashboard.js
@@ -574,7 +574,9 @@ function triggerFileUpload() {
 // ============================================
 
         const BANK_CODES = {
-            'Opay':'305' ,
+            'Opay':'305',
+            'Palmpay':'10033',
+            'Moniepoint':'50515',
             'Access Bank': '025',
             'Zenith Bank': '057',
             'GTBank': '058',
